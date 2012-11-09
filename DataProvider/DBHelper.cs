20e28df257a64defab7ab73877f5b9b12d1d8bca
AndyYou <andyyu0920@gmail.com>
2012-11-09 13:41:28 +0800

    DataHelper and Login method

---
+++ DataProvider/DBHelper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SqlClient;
+
+namespace DataProvider
+{
+    public static class DBHelper
+    {
+        public static SqlConnection GetConnection()
+        {
+            string str = "Data Source=WIN-APPUTU; Initial Catalog=Andy; Persist Security Info=True;User ID=sa;Password=apputu.SQL;";
+            SqlConnection conn = new SqlConnection(str);
+
+            return conn;
+        }
+
+        public static string GetConnctionString()
+        {
+            string str = "Data Source=WIN-APPUTU; Initial Catalog=Andy; Persist Security Info=True;User ID=sa;Password=apputu.SQL;";
+            return str;
+        }
+
+    }
+}
---
+++ DataProvider/Employee.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Linq;
+using System.Data.Linq;
+using System.Data.Linq.Mapping;
+
+
+namespace DataProvider
+{
+    
+    [Table(Name = "Employee")]
+    public class CEmployee
+    {
+        [Column(IsDbGenerated=false, IsPrimaryKey=true)]
+        public Int32 EId;
+
+        [Column]
+        public String Account;
+
+        [Column]
+        public String Password;
+
+        [Column(IsDbGenerated=true)]
+        public DateTime LastLoginDate;
+
+        [Column]
+        public String Rank;
+
+        [Column]
+        public String Name;
+
+        [Column]
+        public String Email;
+
+        [Column]
+        public Boolean Activity;
+
+    }
+}
