66f20a2c0ee8a631fffcb7d413cd7e3c85935441
AndyYou <andyyu0920@gmail.com>
2012-12-26 10:16:16 +0800

    Fixed select duplicate

---
+++ DataProvider/SystemConfig.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.Linq.Mapping;
+
+namespace DataProvider
+{
+    [Table(Name = "SystemConfig")]
+    public class SystemConfig
+    {
+        [Column(Name="FtpHost")]
+        public String FtpHost { set; get; }
+        [Column(Name = "FtpPort")]
+        public Int32 FtpPort { set; get; }
+        [Column(Name = "FtpUsername")]
+        public String FtpUsername { set; get; }
+        [Column(Name = "FtpPassword")]
+        public String FtpPassword { set; get; }
+        [Column(Name = "FtpComponentCode")]
+        public String FtpComponentCode { set; get; }
+        [Column(Name = "ApiHost")]
+        public String ApiHost { set; get; }
+        [Column(Name = "ApiPort")]
+        public Int32 ApiPort { set; get; }
+        [Column(Name = "ApiKey")]
+        public String ApiKey { set; get; }
+        [Column(Name = "LimitationDay")]
+        public Int32 LimitationDay { set; get; }
+    }
+}
